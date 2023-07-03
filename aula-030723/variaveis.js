fd2898e21c9974ac9646e2d43effda657f4a6a91
Gabicgodoy <gabycostagodoy@gmail.com>
2023-07-03 12:06:33 -0300

    Variaveis

---
+++ aula-030723/variaveis.js
@@ -0,0 +1,44 @@
+
+// Java -> Linguagem de Programação (Compilada)
+// JavaScript -> Linguagem de Programação(Interpretada)
+
+/*
+Comentários de multiplas linhas
+*/
+// Comentário de única linha
+
+// Chrome -> Interpretador de JS
+// Node.js -> V8 + NPM (Node Package Manager)
+
+console.log("Texto"); //imprime no terminal/console uma informação
+console.log(5);
+
+// Variaveis -> Espaço (na memória) para armazenar um valor
+let nome  // Declaração da varíavel nome
+nome = "Gabriela Godoy"; // Atribuição do valor da varíavel
+console.log(nome);
+
+let idade = 23; // Declaração da varíavel e atribuição valor
+console.log(idade);
+
+let altura = 1.75;
+console.log(altura);
+
+let cpf = "123.456.789-00";
+console.log(cpf);
+
+let casado = true; //true or false
+console.log(casado);
+
+// Substitui o valor 23 por 24
+idade = 24;
+console.log(idade);
+
+const PI = 3.14; // Declara a atribuição de uma constante
+console.log(PI);
+
+//PI - 14; // Isto vai gerar erro
+// console.log(PI)
+
+// Tipagem Estatica -> Definição de um tipo a cada variavel (Java, C, C++, TypeScript)
+// Tipagem Dinamica -> O tipo da variavel é definido de acordo com seu valor (JavaScript, Python)
