dd3ee1a290477f0fff7d348d985b97600368efeb
Gabicgodoy <gabycostagodoy@gmail.com>
2023-07-26 12:25:45 -0300

    .

--- aula-189723/galeria-imagens/script.js
+++ aula-189723/galeria-imagens/script.js
@@ -7,3 +7,17 @@ const imagens = [
     "https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
 ];
 
+const imagem = document.querySelector ("#galeria");
+let indiceAtual = 1;
+
+function galeria() {
+    imagem.src = imagens[indiceAtual]; // acessando e atribuindo a imagem com base no indice atual
+    if(indiceAtual == (imagens.length-1)) {
+        indiceAtual = 0;
+    }
+    else {
+        indiceAtual++;
+    }
+}
+
+setInterval(galeria, 2000);
